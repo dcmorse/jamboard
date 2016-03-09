2506baf44c576fdbd80fda17f75acd692faf8c02
dcmorse <dcmorse@gmail.com>
2016-03-08 20:24:52 -0600

    sqaure one

---
+++ jamboard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jamboard.Startup))]
+namespace jamboard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
