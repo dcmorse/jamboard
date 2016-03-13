e786233f262deb295ae9aa43b4d490b5a30c1594
dcmorse <dcmorse@gmail.com>
2016-03-13 19:15:28 -0400

    Edit Skater now allows editing of team. closes: https://github.com/dcmorse/jamboard/issues/1

--- jamboard/Controllers/SkatersController.cs
+++ jamboard/Controllers/SkatersController.cs
@@ -78,7 +78,7 @@ namespace jamboard.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Number")] Skater skater)
+        public ActionResult Edit([Bind(Include = "Id,Name,Number,TeamId")] Skater skater)
         {
             if (ModelState.IsValid)
             {
