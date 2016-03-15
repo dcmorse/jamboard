cf68568bd67e851272eb90789f5f2949b41378ca
dcmorse <dcmorse@gmail.com>
2016-03-15 18:08:51 -0400

    tighten permissions on SkaterController.Create

--- jamboard/Controllers/SkatersController.cs
+++ jamboard/Controllers/SkatersController.cs
@@ -46,7 +46,7 @@ namespace jamboard.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Number,TeamId")] Skater skater)
+        public ActionResult Create([Bind(Include = "Name,Number,TeamId")] Skater skater)
         {
             if (ModelState.IsValid)
             {
